64efb6a4e9fdf923e79d356c5d2ae2a0a2e3753d
cesar sabater <cesar.sabater@cgm.com>
2020-02-18 16:31:15 +0100

    first basic working impl of user creation / retreival in python project

---
+++ users/web/app/routes.py
@@ -0,0 +1,61 @@
+from app import app, db
+from app.models import User, UserSchema
+from flask import jsonify, request
+import os
+
+user_schema = UserSchema()
+
+
+@app.route("/")
+def index():
+    return "\n* * *\n* Y *\n* O *\n* L *\n* O *\n* * *\n"
+
+@app.route("/user", methods=['POST'])
+def user_details():
+    data = request.get_json()
+    if data['full_name'] and data['email'] and data['location']:
+        existing_user = User.query.filter_by(email=data['email']).first()
+        if existing_user is not None:
+            response = {
+                'message': 'user already exists'
+            }
+            return jsonify(response), 403
+        new_user = User(
+            full_name=data['full_name'], email=data['email'], location=data['location'])
+        db.session.add(new_user)
+        db.session.commit()
+
+        user = User.query.filter_by(full_name=data['full_name']).first()
+
+        response = {
+            'user_id': user.id,
+            'message': 'new user registered',
+
+        }
+        return jsonify(response), 202
+    else:
+        response = {
+            'status': 'error',
+            'message': ['bad request body']
+        }
+    return jsonify(response), 400
+
+
+@app.route("/user/<user_id>", methods=['GET'])
+def get_user_details(user_id):
+    user = User.query.filter_by(id=user_id).first()
+    if user is None:
+        response = {
+            'message': 'user does not exist'
+        }
+        return jsonify(response), 404
+    result = user_schema.dumps(user).data
+    response = {
+        'data': result,
+        'status_code': 202
+    }
+    return jsonify(response)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
